88040bdf852605c704d8137a8c74c85217d51622
olkinin <olkinin@mail.ru>
2023-03-08 16:11:01 +0300

    commit

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 create table students
 (id bigserial primary key, name varchar(255),age int);
 insert into students (name, age)
-values ('Bob', 30), ('Jon', 25), ('Makl', 28);
+values ('Bob', 30), ('Jon', 25), ('Makl', 28),('Ivan', 23);
 
